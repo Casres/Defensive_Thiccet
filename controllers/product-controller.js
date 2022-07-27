107d3a81fab0c89991ba044a66f671c64ff87ac2
Casres <christian@casillas.tech>
2022-07-27 17:33:46 -0400

    Added a update/delete product route

--- controllers/product-controller.js
+++ controllers/product-controller.js
@@ -27,10 +27,41 @@ const productController = {
         res.status(404).json(err);
       });
   },
-
+  // created a product
   createProduct({ body }, res) {
-    product.create(body)
+    product
+      .create(body)
       .then((dbProductsData) => res.json(dbProductsData))
+      .catch((err) => res.status(400).json(err));
+  },
+
+  // updates a pizza by id
+  updateProduct({ params }, res) {
+    product
+      .findOneAndUpdate({ _id: params.id }, body, { new: true })
+      .then((dbProducts) => {
+        if (dbProductsData) {
+          res
+            .status(404)
+            .json({
+              message: `No product found with this id, please try again.`,
+            });
+          return;
+        }
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+
+  // deletes a product by id
+  deleteProduct({ params }, res) {
+    products.findOneAndDelete({ _id: params.id })
+      .then((dbProductsData) => {
+        if (!dbProductsData) {
+          res.status(404)
+          .json({message: "No product found with this id, please try again"});
+          return;
+        }
+      })
       .catch((err) => res.status(400).json(err));
   },
 };
