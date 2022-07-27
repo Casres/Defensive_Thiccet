a6fde83462edfa3c00deb9dd78096e3ce66986ec
Casres <christian@casillas.tech>
2022-07-27 19:53:47 -0400

    imported Schema and model from mongoose for the pizza.js model

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
-const express = require("express"); 
-const router = express.Router(); 
-const htmlRoutes = require("./html"); 
-const apiRoutes = require("./api/index"); 
- 
-router.use("/", htmlRoutes); 
-router.use("/api", apiRoutes); 
+const express = require("express");
+const router = express.Router();
+const htmlRoutes = require("./html");
+const apiRoutes = require("./api/index");
 
-res.status(404).send(`<h1> 🤖 404 Error </h1>`); 
- 
-module.exports = router; 
+router.use("/", htmlRoutes);
+router.use("/api", apiRoutes);
+
+router.use((req, res) => {
+  res.status(404).send(`<h1> 🤖 404 Error </h1>`);
+});
+
+module.exports = router;
