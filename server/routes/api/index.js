9ee65463c4d6ad3cf37ba00a5527c43635603ad0
Beepo <micahstew@gmail.com>
2022-07-28 18:36:12 -0400

    fixing header

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,6 +1,11 @@
 const router = require('express').Router(); 
+const path = require('path')
 const productRoutes = require('./product-routes'); 
 
 router.use('/products', productRoutes);  
+
+router.use((req, res) => {
+    res.sendFile(path.join(__dirname, '../../client/public/index.html'));
+  });
 
 module.exports = router;  
