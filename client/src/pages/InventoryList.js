a632334225c896ba91f039ee3478a424c6f74eb2
Beepo <micahstew@gmail.com>
2022-07-28 18:52:50 -0400

    adding dummy data

--- client/src/pages/InventoryList.js
+++ client/src/pages/InventoryList.js
@@ -54,13 +54,13 @@ return (
         </div>
         <div key="{item._id}" className="card mb-3">
           <p className="card-header">
-            Item Name
+            water bottle 
           </p>
           <div className="card-body">
-          <div>Price:</div>
-          <div>Quantity:</div>
-          <div>Size:</div>
-          <div>brand:</div>
+          <div>Price: $400,000,000,000</div>
+          <div>Quantity:3/4</div>
+          <div>Size: Medium</div>
+          <div>brand: Gator world</div>
           </div>
         </div>
         <div key="{item._id}" className="card mb-3">
