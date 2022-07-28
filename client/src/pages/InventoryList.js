881b3792239d979cabd97dad5e00c8c114d85c41
Beepo <micahstew@gmail.com>
2022-07-28 17:08:30 -0400

    adding cards

--- client/src/pages/InventoryList.js
+++ client/src/pages/InventoryList.js
@@ -2,33 +2,48 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const InventoryList = ({ items, title }) => {
-    if (!items.length) {
-      return <h3>No Inventory Yet</h3>;
-    }
+
   
-    return (
-      <div>
-        <h3>{title}</h3>
-        {items &&
-          items.map(item => (
-            <div key={item._id} className="card mb-3">
-              <p className="card-header">
-                item on {item.createdAt}
-              </p>
-              <div className="card-body">
-                <Link to={`/item/${item._id}`}>
-                  <p>{item.itemText}</p>
-                  <p className="mb-0">
-                    Items: {item.itemCount} || Click to{' '}
-                    {item.itemCount ? 'see' : 'start'} the discussion!
-                  </p>
-                </Link>
-              </div>
-            </div>
-          ))}
-      </div>
-    );
-  };
+    // return (
+    //   <div>
+    //     <h3>{title}</h3>
+    //     {items &&
+    //       items.map(item => (
+    //         <div key={item._id} className="card mb-3">
+    //           <p className="card-header">
+    //             item on {item.createdAt}
+    //           </p>
+    //           <div className="card-body">
+    //             <Link to={`/item/${item._id}`}>
+    //               <p>{item.itemText}</p>
+    //               <p className="mb-0">
+    //                 Items: {item.itemCount} || Click to{' '}
+    //                 {item.itemCount ? 'see' : 'start'} the discussion!
+    //               </p>
+    //             </Link>
+    //           </div>
+    //         </div>
+    //       ))}
+    //   </div>
+
+return (
+  <div>
+    <h3>{title}</h3>
+        <div key="{item._id}" className="card mb-3">
+          <p className="card-header">
+            Item Name
+          </p>
+          <div className="card-body">
+          <div>Price:</div>
+          <div>Quantity:</div>
+          <div>Size:</div>
+          <div>brand:</div>
+          </div>
+        </div>
+  </div>
+);
+}
+  
   
   
 
